595220333d2b79d55ebaf338f265c25294c0961d
khoidaoo <khoi.dao2812@gmail.com>
2024-05-18 17:14:59 +0700

    feat(ecommerce): add new project into portfolio

--- src/pages/Work/data.ts
+++ src/pages/Work/data.ts
@@ -1,4 +1,5 @@
 import portfolio from "../../assets/portfolio.jpg";
+import ecommerce from "../../assets/ecommerce.png";
 
 export const projects = [
   {
@@ -6,6 +7,13 @@ export const projects = [
     images: [`${portfolio}`],
     sourceCodeLink: "#",
     demoLink: "https://react-portfolio-umber-two.vercel.app/",
+    type: "react",
+  },
+  {
+    title: "Ecommerce",
+    images: [`${ecommerce}`],
+    sourceCodeLink: "#",
+    demoLink: "https://ecomerce-ui-five.vercel.app/",
     type: "react",
   },
 ];
